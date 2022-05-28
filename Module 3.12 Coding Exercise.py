08473b350267f361f0b83710cb96ee0504434d3b
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-28 15:58:40 +0700

    Seventh Exercise

---
+++ Module 3.12 Coding Exercise.py
@@ -0,0 +1,29 @@
+#1).using raise exception
+#x = 500
+#if x > 100:
+#    raise Exception("x is not bigger than 100")
+
+#2).print(variable_1) using try and except block
+#try:
+#    print(variable_1)
+#except:
+#    print("variable_1 is not defined")
+
+#3).using range
+#for i in range (6):
+#    print("I'm so happy!")
+
+#4).using else keyword
+#try:
+#    print(12*6)
+#except:
+#    print("This operation can't be performed")
+#else:
+#    print("This operation can be performed")
+
+#5).using assertion
+#best_burger = "Burger King"
+#number_2_burger = "McDonald's"
+
+#assert best_burger == "Burger King"
+#assert best_burger == "McDonld's"

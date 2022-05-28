19e313f315ca0dc2b3b754502dd53e5c9c5a1e4d
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-28 15:55:52 +0700

    Fifth Exercise

---
+++ Module 3.4 Coding Exercise.py
@@ -0,0 +1,8 @@
+f = open("samplefile.txt", "w")
+f.write("Its easy to learn")
+f = open("samplefile.txt", "a")
+f.write("\nCreate a simple program")
+f.write("\nTry to create a simple program")
+f.write("\nTo learn programming")
+f = open("samplefile.txt", "r")
+f.close()

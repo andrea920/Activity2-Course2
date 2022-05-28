d6bdef449bbec8bf913fa7f5a4a8cb8dc947f7fe
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-28 16:01:48 +0700

    Eighth Exercise

---
+++ Module 4.4 Coding Exercise/functionfile.py
@@ -0,0 +1,10 @@
+def code_name(code_name):
+    print("My Codename is " + code_name + "!")
+def skills(skills):
+    print("I can " + skills + " as my skill")
+def favorite_color(favorite_color):
+    print(favorite_color + "is my favoite color")
+def motto(motto):
+    print(motto + "is my motto in life")
+def gender(gender):
+    print("I'm" + gender)
---
+++ Module 4.4 Coding Exercise/main.py
@@ -0,0 +1,5 @@
+from functionfile import code_name
+from functionfile import skills
+
+code_name("Strong girl")
+skills("generate my muscle")
